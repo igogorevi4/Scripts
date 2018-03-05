9fc0e49cd52b6d2ea0d23ce22d4b69df942d9d26
root <root@DESKTOP-1ITCTJC.localdomain>
2018-03-05 13:34:22 +0300

    fixup docs

--- check.limits.sh
+++ check.limits.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# check if limits work which has been set up
+# check if limits work which have been set up
 for pid in `pidof nginx`; do echo "$(< /proc/$pid/cmdline)"; egrep 'files|Limit' /proc/$pid/limits; echo "Currently open files: $(ls -1 /proc/$pid/fd | wc -l)"; echo; done
