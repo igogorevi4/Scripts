8ea14617b0b8c34033410ac06b48ff1facf2928c
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2018-08-30 11:52:03 +0300

    fix getting PATH to cron script

--- clickhouse.daily.backup.sh
+++ clickhouse.daily.backup.sh
@@ -1,28 +1,26 @@
-#!/bin/bash
+#!/bin/bash -l
 
+CLICKHOUSE=$(/usr/bin/clickhouse-server)
 DATE=$(/bin/date +\%Y\%m\%d\%H)
 DATADIR='/opt/clickhouse/data/'
 BACKUPDIR='/opt/clickhouse/backups'
-HOST=$(host -t axfr $(hostname -i | awk '{print $NF}') | grep PTR | awk '{print $NF}' | grep com)
+HOST=$(host -t axfr $(hostname -i | awk '{print $NF}') | grep PTR | awk '{print $NF}' | grep ch)
 PORT=$(grep tcp_port /etc/clickhouse-server/config.xml | awk '{gsub(/[^0-9. ]/,"")}1')
-BASES=$(clickhouse-server --client --port $PORT --host $HOST --query "show databases;" | grep -vE "default|system")
+BASES=$($CLICKHOUSE --client --port $PORT --host $HOST --query "show databases;" | grep -vE "default|system")
 
 # Freeze pratitions
 for BASE in $BASES; do
-        TABLES=$(clickhouse-server --client --port $PORT --host $HOST --query "SELECT table FROM system.parts WHERE active AND database = '$BASE';"|sort -u)
+#	echo $BASE;
+	TABLES=$($CLICKHOUSE --client --port $PORT --host $HOST --query "SELECT table FROM system.parts WHERE active AND database = '$BASE';"|sort -u)
         for table in $TABLES; do
-                clickhouse-server --client --port $PORT --host $HOST --database $BASE --query "ALTER TABLE $table FREEZE PARTITION '2';"
+#		echo $table;
+                $CLICKHOUSE --client --port $PORT --host $HOST --database $BASE --query "ALTER TABLE $table FREEZE PARTITION '2';"
         done;
 done;
 
-# Copy backups & metadata
-if mkdir $BACKUPDIR/$DATE && chown -R clickhouse:clickhouse $BACKUPDIR/$DATE 
-       then cp -r $DATADIR/shadow $BACKUPDIR/$DATE
-fi 
-if mkdir $BACKUPDIR/$DATE_metadata && chown -R clickhouse:clickhouse $BACKUPDIR/$DATE_metadata
-       then cp -r $DATADIR/metadata $BACKUPDIR/$DATE_metadata
-fi
+echo "Freezing has been finished"
 
 # Delete obsolete backups
-find $DATADIR/shadow/ -mtime +7 -delete
-find $BACKUPDIR/ -mtime +7 -delete
+echo "Deleting obsolete shadow dir"
+find $DATADIR/shadow/ -mtime +7 -exec rm -r "{}" \;
+echo "DONE!"
